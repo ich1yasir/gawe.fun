319eeaecd24596733fb2bc21f7d83195f9a57d44
ich1yasir <ichwan.yasir@jonajo.com>
2022-01-27 18:03:20 +0700

    Update Box

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,63 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react';
+import { GpsFixed } from '@mui/icons-material';
 
 export default function Home() {
+
+  const [boxes, setBoxes] = useState([]);
+  function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  function randomGreyHex() {
+    var v = (Math.random()*(230)|150).toString(16);//bitwise OR. Gives value in the range 0-255 which is then converted to base 16 (hex).
+    return "#" + v + v + v;
+  }
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  const generateRandom = () => {
+    var styelJSList = []
+    var row = -400
+    for (let i = 0; i < 35; i++) {
+      const topMargin = row + getRandom(30, 60)
+      const widthR = getRandom(10, 100)
+      const heightR = getRandom(5, 15)
+      const zVar= getRandom(5, 30)
+      const data = {
+        key: i+'',
+        margin: topMargin + 'px 10px 10px -10%',
+        zIndex: zVar+'',
+        position: 'fixed',
+        width: widthR + '%',
+        height: heightR + 'rem',
+        backgroundColor: randomGreyHex()
+      }
+      row = topMargin
+      styelJSList.push(data)
+    }
+    setBoxes(shuffle(styelJSList));
+  }
+  useEffect(() => {
+    // Update the document title using the browser API
+    generateRandom();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,18 +66,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+
+      <div className={styles.background_boxes}>
+        {boxes.map((box, index) => (
+          <div key={index} className={styles.box1} style={box}>
+
+          </div>
+        ))}
+
+
+      </div>
+      <main className={styles.main} style={{zIndex: 100}}>
         <h1 className={styles.title}>
-          Coming <a href="#">Soon</a>
+          Gawe <a href="#">Fun</a>
         </h1>
 
         <p className={styles.description}>
-          <code className={styles.code}>few days left</code>
+          <code className={styles.code}>Kumpulan aplikasi sederhana</code>
         </p>
 
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={styles.footer} style={{zIndex: 100}}>
         <a
           href="#"
           target="_blank"
@@ -31,7 +96,7 @@ export default function Home() {
           By : One-Team
         </a>
         <a>
-        <code className={styles.code}> @GlobalOne Project</code>
+          <code className={styles.code}> @GlobalOne Project</code>
         </a>
       </footer>
     </div>
