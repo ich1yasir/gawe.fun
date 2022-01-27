bda865c09dda608d15304a17d5207e5aee3f565b
user <user@DESKTOP-SMOAP8C>
2022-01-27 10:48:48 +0700

    Update Hadist

--- pages/hadist/search.js
+++ pages/hadist/search.js
@@ -1,9 +1,20 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
+import { styled, alpha } from '@mui/material/styles';
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import LocalLibraryIcon from '@mui/icons-material/LocalLibrary';
+import Container from '@mui/material/Container';
 import Head from 'next/head'
 import axios from 'axios';
 import styles from '../../styles/Home.module.css'
-import { Button, CircularProgress, LinearProgress } from '@mui/material';
+import { Button, CircularProgress, colors, IconButton, Paper, Icon } from '@mui/material';
 
 const CardAyat = ({ ayat }) => {
     return <div className={styles.ayat}>
@@ -13,7 +24,62 @@ const CardAyat = ({ ayat }) => {
     </div>;
 }
 
-const QueryPage = () => {
+function ElevationScroll(props) {
+    const { children, window } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+        disableHysteresis: true,
+        threshold: 0,
+        target: window ? window() : undefined,
+    });
+
+    return React.cloneElement(children, {
+        elevation: trigger ? 4 : 0,
+    });
+}
+
+ElevationScroll.propTypes = {
+    children: PropTypes.element.isRequired,
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+};
+
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
+    background: theme.palette.common.white
+}));
+
+const StyledForm = styled('form')(({ theme }) => ({
+    width: '100%'
+}));
+
+const StyledDiv = styled('div')(({ theme }) => ({
+    margin: 'auto',
+    width: '100%',
+    maxWidth: '900px'
+}));
+
+const StyledTitle = styled(Typography)(({ theme }) => ({
+    color: '#0eac00',
+    marginRight: '2rem',
+    fontSize: 'xx-large',
+    fontWeight: '900'
+
+}));
+
+
+const preventDefault = f => e => {
+    e.preventDefault()
+    f(e)
+}
+
+
+export default function QueryPage(props) {
+    const [query, setQuery] = useState("");
     const [hadist, setHadist] = useState([]);
     const [loading, setLoading] = useState(false);
     const [loadMore, setLoadMore] = useState(false);
@@ -30,11 +96,11 @@ const QueryPage = () => {
                 .then(res => {
                     const listHadis = res.data;
                     setHadist([...hadist, ...listHadis]);
-                    const nextPage = page+1;
+                    const nextPage = page + 1;
                     setPage(nextPage)
-                    if (listHadis.length < 10){
+                    if (listHadis.length < 10) {
                         setLoadMore(false);
-                    }else{
+                    } else {
                         setLoadMore(true);
                     }
                     setLoading(false)
@@ -42,46 +108,81 @@ const QueryPage = () => {
         }
     }
 
+    const handleSubmit = preventDefault(() => {
+        setHadist([]);
+        router.push({
+            pathname: "/hadist/search",
+            query: { q: query },
+        })
+    })
+
     useEffect(() => {
         setLoading(true)
+        setQuery(q)
         // Update the document title using the browser API
         loadHadist();
     }, [q]);
 
     return (
-
-        <div>
+        <React.Fragment>
             <Head>
                 <title>Referensi Islam</title>
                 <meta name="description" content="Mencari hukum islam cari hadist dan ayat alquran hanya ada di Referensi Islam" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className={styles.main_hadist}>
-                <h1 className={styles.title_hadist}>
-                    Referensi <a>Islam!</a>
-                </h1>
-
-                <div className={styles.grid}>
-                    <div>
-                        {hadist && hadist.map((obj, index) =>
-                            <CardAyat key={index.toString()} ayat={obj} />
-                        )}
+            <ElevationScroll {...props}>
+                <StyledAppBar>
+                    <Toolbar>
+                        <StyledDiv sx={{display: 'flex', alignItems: 'center'}}>
+                            <StyledTitle>
+                                Hadist
+                            </StyledTitle>
+                            <StyledForm onSubmit={handleSubmit}>
+                                <Paper
+                                    sx={{ p: '2px 4px', display: 'flex', alignItems: 'center' }}
+                                >
+                                    <Icon sx={{ m: '10px' }} >
+                                        <LocalLibraryIcon />
+                                    </Icon>
+                                    <InputBase
+                                        sx={{ ml: 1, flex: 1 }}
+                                        placeholder="Cari Referensi Tentang: Mendidik anak"
+                                        inputProps={{ 'aria-label': 'Cari Referensi tentang' }}
+                                        value={query}
+                                        onChange={e => setQuery(e.target.value)}
+                                    />
+                                    <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                                        <SearchIcon />
+                                    </IconButton>
+                                </Paper>
+                            </StyledForm>
+                        </StyledDiv>
+                    </Toolbar>
+                </StyledAppBar>
+            </ElevationScroll>
+            <Toolbar />
+            <Container>
+                <main className={styles.main_hadist}>
+                    <div className={styles.grid}>
+                        <div>
+                            {hadist && hadist.map((obj, index) =>
+                                <CardAyat key={index.toString()} ayat={obj} />
+                            )}
+                        </div>
+                        {
+                            loading ? <CircularProgress /> : (null)
+                        }
+                        {loadMore && <Button onClick={() => loadHadist()}>Load More..</Button>}
                     </div>
-                    {
-                        loading ? <CircularProgress /> : (null)
-                    }
-                    {loadMore && <Button onClick={() => loadHadist()}>Load More..</Button>}
-                </div>
-            </main>
+                </main>
 
-            <footer className={styles.footer_hadist}>
-                Powered by{' '}
-                <span className={styles.logo}>
-                    <a><i>One team</i></a>
-                </span>
-            </footer>
-        </div>
+                <footer className={styles.footer_hadist}>
+                    Powered by{' '}
+                    <span className={styles.logo}>
+                        <a><i>One team</i></a>
+                    </span>
+                </footer>
+            </Container>
+        </React.Fragment>
     )
 }
-
-export default QueryPage
